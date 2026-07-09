061bb61aa1feae62d377705f43ebecbc57631dfa
Camila-Vertiz <camila.vrtz@gmail.com>
2026-07-09 00:37:29 -0500

    BankingDbContext

---
+++ Banking.Infrastructure/Data/BankingDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Banking.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Banking.Infrastructure.Data
+{
+    public class BankingDbContext : DbContext
+    {
+        public BankingDbContext(DbContextOptions<BankingDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<BankAccount> Accounts => Set<BankAccount>();
+        public DbSet<Transaction> Transactions => Set<Transaction>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BankingDbContext).Assembly);
+        }
+
+
+    }
+}
